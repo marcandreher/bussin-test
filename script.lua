21bd1b6905e443782019acc71a1f50455168c2c8
marcandreher <herpersmarcandre3@gmail.com>
2024-06-01 16:13:42 +0200

    ref: script

--- script.lua
+++ script.lua
@@ -2,8 +2,11 @@ local textbox = get("textbox")
 local info = get("info")
 
 textbox.on_input(function(content)
-    if(string.len(content) < 6) then
-        info.set_content("Password must be at least 6 characters long")
-        return
+    textbox.on_input(function (content)
+        if #content < 6 then
+            info.set_content("Password must be at least 6 characters long")
+            return
+        end
+        info.set_content("You BEAT the BUSS PASSWORD GAME!")
     end
 end)
