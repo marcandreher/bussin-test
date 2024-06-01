7ebc2f810251c54cf5c27c037ad4ebf78f17332f
marcandreher <herpersmarcandre3@gmail.com>
2024-06-01 16:20:01 +0200

    add: protocol step

--- script.lua
+++ script.lua
@@ -6,5 +6,9 @@ textbox.on_input(function(content)
         info.set_content("Password must be at least 6 characters long | current length: " .. #content)
         return
     end
+    if not string.match(content, "buss://") then
+        info.set_content("The Password needs to contain the Bussin Web X default Protocol")
+        return
+    end
     info.set_content("You BEAT the BUSS PASSWORD GAME!")
 end)
