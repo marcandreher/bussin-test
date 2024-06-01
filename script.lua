1ed86e1cf9b00be9622d44f48b9b653c42d8405f
marcandreher <herpersmarcandre3@gmail.com>
2024-06-01 16:40:51 +0200

    fix: browser question

--- script.lua
+++ script.lua
@@ -2,6 +2,7 @@ local textbox = get("textbox")
 local info = get("info")
 
 textbox.on_input(function(content)
+    local contentUp = string.upper(content)
     if #content < 6 then
         info.set_content("Password must be at least 6 characters long | current length: " .. #content)
         return
@@ -10,7 +11,6 @@ textbox.on_input(function(content)
         info.set_content("The Password needs to contain the Bussin Web X default Protocol")
         return
     end
-    local contentUp = string.upper(content)
     if not string.match(contentUp, "LUA") then
         info.set_content("The Password needs to contain the Language that replaces javascript in this browser")
         return
@@ -19,7 +19,7 @@ textbox.on_input(function(content)
         info.set_content("Math Question: What are the first three digits of Pi after the decimal point?")
         return
     end
-    if not string.match(contentUp, "WEBX") or not string.match(contentUp, "WEB X") then
+    if not (string.match(contentUp, "WEBX") or string.match(contentUp, "WEB X")) then
         info.set_content("The Password needs to contain the name of the Browser")
         return
     end
